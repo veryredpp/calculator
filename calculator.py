3dd9d86ec449f202361434fe05efa54c81630db8
veryredpp <panagiotis.kouzaris@gmail.com>
2022-01-01 16:53:27 +0200

    This is the actual thing

---
+++ calculator.py
@@ -0,0 +1,18 @@
+def menu():
+    print('Choices:\n1. Addition\nMore coming soon!')
+
+print('Welcome to the best calculator!\n')
+menu(), print("\n")
+
+choice = int(input('Choice: '))
+if choice < 1 or choice > 1:
+    print('Invalid Selection')
+
+
+x = int(input('Choose your first number: '))
+y = int(input('Choose your second number: '))
+
+#Addition
+if choice == 1:
+    print('{} + {} = '.format(x, y))
+    print(x+y)
