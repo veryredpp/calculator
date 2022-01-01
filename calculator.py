30e36d17b3880f60149edecd22b950999115a3de
veryredpp <panagiotis.kouzaris@gmail.com>
2022-01-01 20:38:16 +0200

    This is the actual thing

--- calculator.py
+++ calculator.py
@@ -1,18 +1,34 @@
-def menu():
-    print('Choices:\n1. Addition\nMore coming soon!')
-
 print('Welcome to the best calculator!\n')
-menu(), print("\n")
+endchoice = ""
+while endchoice == "":
+    def menu():
+        print('Choices:\n1.Addition\n2.Subtraction\n3.Multiplication\n4.Division')
 
-choice = int(input('Choice: '))
-if choice < 1 or choice > 1:
-    print('Invalid Selection')
+    print("\n")
+    menu(), print("\n")
 
-if choice == 1:
-    x = int(input('Choose your first number: '))
-    y = int(input('Choose your second number: '))
+    choice = int(input('Choice: '))
+    while choice < 1 or choice > 4:
+        print('Invalid Selection')
+        choice = int(input('Choice: '))
 
-#Addition
-if choice == 1:
-    print('{} + {} = '.format(x, y))
-    print(x+y)
+    if choice >= 1 and choice <= 4 :
+        x = int(input('Choose your first number: '))
+        y = int(input('Choose your second number: '))
+
+    #Addition
+    if choice == 1:
+        print('{} + {} ='.format(x,y),x+y)
+
+    #Subtraction
+    if choice == 2:
+        print('{} - {} ='.format(x,y),x-y)
+
+    #Multiplication
+    if choice == 3:
+        print('{} * {} ='.format(x,y),x*y)
+    #Division
+    if choice == 4:
+        print('{} / {} ='.format(x,y),x/y)
+    print('\n')
+    endchoice = input('Press Enter to continue...')
